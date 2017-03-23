0970a30a6ce39ce7bc03a8341ec28dc434e2be57
LICUI439 <LICUI439@pingan.com.cn>
2017-03-23 13:50:45 +0800

    更新缓存键名

--- sw_test/sw.js
+++ sw_test/sw.js
@@ -1,6 +1,8 @@
 this.addEventListener('install', function(event) {
   event.waitUntil(   //在安装之前需要执行的操作
-    caches.open('v2').then(function(cache) {  //建立一个键名为V1的缓存，添加缓存资源列表
+    console.log("安装完成之前需要执行的操作");
+    caches.open('v3').then(function(cache) {  //建立一个键名为V1的缓存，添加缓存资源列表
+      console.log("添加缓存列表 cache.addAll");
       return cache.addAll([
         './',
         './index.html',
@@ -20,10 +22,12 @@ this.addEventListener('fetch', function(event) {
   //caches.match(event.request) 允许我们对网络请求的资源和 cache 里可获取的资源进行匹配，查看是否缓存中有相应的资源
   event.respondWith(caches.match(event.request).catch(function() {
     //如果没有从缓存中匹配成功，则通过fetch请求网络资源
+    console.log("缓存匹配失败");
     return fetch(event.request);
   }).then(function(r) {
     response = r;
-    caches.open('v2').then(function(cache) {    //匹配成功，
+    console.log("缓存匹配成功！");
+    caches.open('v3').then(function(cache) {    //匹配成功，
       cache.put(event.request, response);
     });
     return response.clone();
@@ -35,11 +39,13 @@ this.addEventListener('fetch', function(event) {
 
 //清理旧缓存
 this.addEventListener('activate', function(event) {
-  var cacheWhitelist = ['v2'];   //当前新的缓存   删除除此之外的缓存
+  var cacheWhitelist = ['v3'];   //当前新的缓存   删除除此之外的缓存
 
   event.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
+        console.log("清理旧缓存key");
+        console.log(key);
         if (cacheWhitelist.indexOf(key) === -1) {
           return caches.delete(key);
         }
