8b13739787308d6138cacc4900ab653434e70cbe
LICUI439 <LICUI439@pingan.com.cn>
2017-11-07 14:19:25 +0800

    测试目录

---
+++ testpwa/js/index.js
@@ -0,0 +1,75 @@
+
+
+var common = {
+        //domid:加载子页面的父dom选择器
+        //htmlPage：加载子页面html的文件路径
+        //callback：回调方法
+        fLoadPage: function(domid, htmlPage, callback) { //加载子页面及js
+            if (typeof(callback) == 'function') {
+                $(domid).load(htmlPage, callback);
+            } else {
+                $(domid).load(htmlPage);
+            }
+        },
+        /*
+         *加载模块，html与js文件名相同
+         *domId:容器Id名称,加载html的容器
+         *module:模块名称
+         */
+        /*fLoadModule: function(domId, module, params) {
+            if (!module) {
+                PAS.CommonAPI.fAlert("", "模块不能为空", "");
+                return;
+            }
+            $(domId).load(module + ".html", function() {
+                require.async("./" + module + ".js", function(mObj) {
+                    if (mObj) {
+                        mObj.initialize(params);
+                    }
+                });
+            });
+        }*/
+
+    };
+
+
+
+
+
+    var APP = {
+        initialize: function(){
+           // common.fLoadPage("#pre-render-zone","./prerender.html");
+            // common.fLoadModule("#wrap","todoList");
+            this.fInstallServiceWorker();
+
+        },
+        fInstallServiceWorker: function(){
+            if(navigator.serviceWorker){
+                console.log("支持service worker");
+
+                // const version = '1.0.4';
+                navigator.serviceWorker.register('/personal/testpwa/sw.js').then(function(reg){
+                    console.log("注册完成后",reg.scope,reg);
+                    //手动更新
+                    // if (localStorage.getItem('sw_version') !== version) {
+                    //     console.log("版本不是最新",version);
+                    //     reg.update().then(() => localStorage.setItem('sw_version', version));
+                    // }
+
+                   if(reg.installing) {
+                      console.log('Service worker installing。。。');
+                    }
+                     if(reg.waiting) {
+                      console.log('Service worker installed。。。');
+                    }
+                     if(reg.active) {
+                      console.log('Service worker active。。。');
+                    }
+                }).catch(function(){
+                    console.log("注册service worker失败");
+                })
+            }
+        }
+    };
+    APP.initialize();
+
---
+++ testpwa/sw.js
@@ -0,0 +1,123 @@
+
+
+//定义缓存的key及缓存列表
+var cacheStorageKey = 'pwa-sw20171107-14';
+var cacheWhitelist = ['pwa-sw20171107-14'];  //清理缓存，保留whitelist中的缓存，其他的删除
+var cacheList = [
+        './pwa.html',
+        './css/prerender.css'
+  ];
+
+self.addEventListener("install",function(e){
+    console.log("sw install 事件触发",e);
+    e.waitUntil( //安装成功后 ServiceWorker 状态会从 installing 变为 installed
+        caches.open(cacheStorageKey)
+        .then(cache => cache.addAll(cacheList))   //cacheList中的文件全部安装完成才能
+        .then(() => self.skipWaiting())  //self.skipWaiting() 页面更新的过程当中, 新的 Service Worker 脚本能立即激活和生效
+    )
+});
+
+
+// 如果希望在有了新版本时，所有的页面都得到及时更新，则在安装阶段跳过等待，直接进入 active
+/*self.addEventListener('install', function(event) {
+    event.waitUntil(self.skipWaiting());
+});*/
+
+self.addEventListener('activate', event => event.waitUntil(
+    Promise.all([
+        // 更新客户端
+        self.clients.claim(),
+        // 清理旧版本
+        caches.keys().then(cacheList => Promise.all(
+            cacheList.map(cacheName => {  console.log("缓存键名",cacheName,cacheWhitelist);
+                //if (cacheName !== CACHE_NAME) {
+                if(cacheWhitelist.indexOf(cacheName)==-1){  console.log("删除老的缓存",cacheName);
+                    caches.delete(cacheName);
+                }
+            })
+        )).catch(function(err){
+            console.log("err",err);
+        })
+    ])
+));
+//更新缓存，清理旧缓存
+/*self.addEventListener("activate",function(e){
+   console.log("清理旧缓存");
+   e.waitUntil(
+        caches.keys().then(function(keyList){
+            return Promise.all(keyList.map(function(key){
+                console.log("缓存列表中的key",key);
+                if(cacheWhitelist.indexOf(key)==-1){
+                    console.log("删除key",key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    )
+});*/
+
+
+
+//监听资源请求  判断是否从缓存中获取
+self.addEventListener("fetch",function(e){
+
+    console.log("fetch事件触发",e);
+    e.respondWith(
+        caches.match(e.request)
+        .then(function(response){  console.log("匹配请求 使用缓存");
+            if(response!=null){
+                return response;
+            }
+            console.log("response null,fetch 网络");
+            return fetch(e.request);   //   fetch(fetchRequest, { credentials: 'include' } ); //fetch请求默认不带cookie，可通过此参数将cookie带过去
+
+        }).catch(function(e){ console.log("fetch 网络");
+            return fetch(e.request);
+        })
+    );
+});
+
+
+//监听离线状态
+/*self.addEventListener('offline', function() {
+    Notification.requestPermission().then(grant => {
+        if (grant !== 'granted') {
+            return;
+        }
+
+        const notification = new Notification("Hi，网络不给力哟", {
+            body: '您的网络貌似离线了，不过在志文工作室里访问过的页面还可以继续打开~',
+            icon: '//lzw.me/images/avatar/lzwme-80x80.png'
+        });
+
+        notification.onclick = function() {
+            notification.close();
+        };
+    });
+});
+*/
+
+//错误监控
+self.onerror = function(errorMessage, scriptURI, lineNumber, columnNumber, error) {
+    if (error) {
+        console.log(error);
+        // reportError(error);
+    } else {
+        console.log(errorMessage, scriptURI, lineNumber, columnNumber, error)
+        // reportError({
+        //     message: errorMessage,
+        //     script: scriptURI,
+        //     line: lineNumber,
+        //     column: columnNumber
+        // });
+    }
+}
+
+//当 Promise 类型的回调发生reject 却没有 catch 处理，会触发 unhandledrejection 事件。
+
+self.addEventListener('unhandledrejection', function(event) {
+    console.log("unhandledrejection",event);
+    // reportError({
+    //     message: event.reason
+    // })
+});
