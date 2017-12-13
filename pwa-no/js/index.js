9f2c9a536be5128e6241752adc503ca62a17fa11
LICUI439 <LICUI439@pingan.com.cn>
2017-12-13 17:41:42 +0800

    非 PWA页面

---
+++ pwa-no/js/index.js
@@ -0,0 +1,18 @@
+
+
+
+
+    var APP = {
+        initialize: function(){
+            // common.fLoadPage("#pre-render-zone","prerender.html");
+            // common.fLoadModule("#wrap","todoList");
+            // this.fInstallServiceWorker();
+            console.log("index.js中initialize.......");
+            // $("#wrap").html("start rendering。。。。");
+            // $("#wrap").show();
+            // $("#pre-render-zone").hide();
+        }
+
+    };
+    APP.initialize();
+
